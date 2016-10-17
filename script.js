a63203deea054e35c9869894fc0b3ddcbd9af43d
mattfoster1 <matt@mfoster.co.uk>
2016-10-17 13:05:24 +0100

    broken URLs fixed

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ var start = function() {
 	}
 }
 
-var generator = function() {
+var generator = function() { //generates bubble elements
 	
 	for (n=0; n < 10; n++) {	
 		var orbCont0 = document.getElementById("orbCont0");
